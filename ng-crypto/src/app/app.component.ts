e6f348ebd0fcbf8b1994f98fec33eada59986c48
tapsa <tapsa872@gmail.com>
2018-02-09 14:24:19 +0200

    Final crypto project

--- ng-crypto/src/app/app.component.ts
+++ ng-crypto/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { DataService } from "./data.service";
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  objectKeys = Object.keys;
+  cryptos: any;
+
+  constructor(private _dataService: DataService) {}
+
+  ngOnInit() {
+    this._dataService.getPrices()
+            .subscribe(res => {
+              this.cryptos = res;
+              console.log(res);
+            });
+  }
 }
